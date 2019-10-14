20c833b9d87902f521e54e622cee4658c7876990
cwkim009 <cwkim@tell-us.co.kr>
2019-10-14 17:56:38 +0900

    타입체크 변경

--- src/main/webapp/extLib/ginno/gutil/GUtil.js
+++ src/main/webapp/extLib/ginno/gutil/GUtil.js
@@ -670,10 +670,10 @@ GUtil = {
 		var res = [];
 		function convert(obj, t){
 			if(!obj) return ;
-			if(typeof obj === 'object'){
+			if(typeof obj === 'object') {
 				var tmp = [];
-				for (var i = 0; i < obj.length; i++){
-					if(typeof obj[i] === 'object'){
+				for (var i = 0; i < obj.length; i++) {
+					if(typeof obj[i] === 'object') {
 						t.push(tmp);
 						convert(obj[i], tmp);
 					}else{
