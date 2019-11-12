2449d5b4a95f8b647e2c48bd61f7651b816cc4c3
cwkim009 <cwkim@tell-us.co.kr>
2019-11-12 17:36:00 +0900

    레이어 수정

--- src/main/webapp/extLib/ginno/gmap/lib/gtool/GTMapLayerTool.js
+++ src/main/webapp/extLib/ginno/gmap/lib/gtool/GTMapLayerTool.js
@@ -474,8 +474,8 @@ GTMapLayerTool = OpenLayers.Class({
 
 						    if (options.layer == undefined ) {
 						        if ( table == "CELL_10" || table == "CELL_SECT" 
-						        	|| table == "MV_ROAD_CELT0002" || table == "MV_ROAD_CELT0005" || table == "MV_ROAD_CELT0012"	 
-						        	|| table == "M_CALS_T" || table == "CELL_10_0301" || table == "CELL_10_4K" || table == "TN_POTHOLE") {
+						        	|| table == "MV_ROAD_CELT0002" || table == "MV_ROAD_CELT0005" || table == "MV_ROAD_CELT0012" || table == "TN_POTHOLE"
+						        	|| table == "M_CALS_T" || table == "CELL_10_0301" || table == "CELL_10_4K") {
 						            arr.push(this.layers[i]);
 						        }
 						    }
